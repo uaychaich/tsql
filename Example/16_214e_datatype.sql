3f87c97a7743743eb24ef0bf97fd987bcd1ce7d1
Uaychai <abc@xyz.com>
2022-12-06 12:48:27 +0700

    Add 16

---
+++ Example/16_214e_datatype.sql
@@ -0,0 +1,8 @@
+--c703 | Basic Data Types | 2022-12-06 | Uaychai Chotjaratwanich
+--Exact Whole Number: TINYINT SMALLINT INT BIGINT BIT
+--Exact Decimal Number: SMALLMONEY MONEY NUMERIC DECIMAL
+--Approximate Decimal Number: FLOAT REAL
+--Date and Time: SMALLDATETIME DATETIME DATETIME2 DATE TIME DATETIMEOFFSET
+--One Language String: CHAR VARCHAR TEXT
+--Multi Language String: NCHAR NVARCHAR NTEXT
+
