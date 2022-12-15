ac0c3141ab61263f0c908cd1b052c3f5065c71de
Uaychai <abc@xyz.com>
2022-12-15 15:31:51 +0700

    2022-12-15

--- Example/16_214e_datatype.sql
+++ Example/16_214e_datatype.sql
@@ -5,8 +5,34 @@
 --Date and Time: SMALLDATETIME DATETIME DATETIME2 DATE TIME DATETIMEOFFSET
 --One Language String: CHAR VARCHAR TEXT
 --Multi Language String: NCHAR NVARCHAR NTEXT
+--https://learn.microsoft.com/en-us/sql/t-sql/data-types/data-types-transact-sql?view=sql-server-ver16
 
 
 --8a29 | Example Exact Whole Number | 2022-12-06 | Uaychai Chotjaratwanich
 DECLARE @Var1 TINYINT = 222;
 SELECT @Var1;
+
+
+--2bbb | Example Exact Decimal Number | 2022-12-15 | Uaychai Chotjaratwanich
+DECLARE @Var2 NUMERIC(20,6) = 222.45626;
+SELECT @Var2;
+
+
+--5c55 | Example Approximate Decimal Number | 2022-12-15 | Uaychai Chotjaratwanich
+DECLARE @Var3 FLOAT = 0.599654522;
+SELECT @Var3;
+
+
+--b145 | Example Date and Time | 2022-12-15 | Uaychai Chotjaratwanich
+DECLARE @Var4 DATETIME2 = '2022-12-15T23:13:45.2356897';
+SELECT @Var4;
+
+
+--a47a | Example One Language String | 2022-12-15 | Uaychai Chotjaratwanich
+DECLARE @Var5 VARCHAR(50) = 'Uaychai';
+SELECT @Var5;
+
+
+--0110 | Example Multi Language String | 2022-12-15 | Uaychai Chotjaratwanich
+DECLARE @Var6 NVARCHAR(50) = 'Uaychai';
+SELECT @Var6;
---
+++ Example/17_b3e0_insert_update_delete_data.sql
@@ -0,0 +1,6 @@
+--8a9b | SELECT ... INTO | 2022-12-15 | Uaychai Chotjaratwanich
+SELECT * INTO Production.CultureCopy FROM Production.Culture;
+SELECT * INTO Sales.SalesOrderHeaderCopy FROM Sales.SalesOrderHeader;
+SELECT * INTO Sales.SalesOrderDetailCopy FROM Sales.SalesOrderDetail;
+
+
