736aad6b9b04604f19bcb00701d1b120f4502cce
Uaychai Chotjaratwanich <abc@xyz.com>
2022-12-15 22:36:21 +0700

    2022-12-15

--- Example/17_b3e0_insert_update_delete_data.sql
+++ Example/17_b3e0_insert_update_delete_data.sql
@@ -1,6 +1,19 @@
 --8a9b | SELECT ... INTO | 2022-12-15 | Uaychai Chotjaratwanich
-SELECT * INTO Production.CultureCopy FROM Production.Culture;
-SELECT * INTO Sales.SalesOrderHeaderCopy FROM Sales.SalesOrderHeader;
-SELECT * INTO Sales.SalesOrderDetailCopy FROM Sales.SalesOrderDetail;
+SELECT * INTO #Culture FROM Production.Culture;
+SELECT * INTO #SalesOrderHeader FROM Sales.SalesOrderHeader;
+SELECT * INTO #SalesOrderDetail FROM Sales.SalesOrderDetail;
 
+
+--5be2 | INSERT new rows | 2022-12-15 | Uaychai Chotjaratwanich
+INSERT #Culture VALUES ('uo','Uaychai',SYSDATETIME()),('yd','Yoda',SYSDATETIME());
+INSERT #Culture(CultureID,[Name],ModifiedDate) VALUES ('pp','PointPoint',SYSDATETIME());
+INSERT #Culture([Name],CultureID,ModifiedDate) VALUES ('TingTing','tt',SYSDATETIME());
+
+
+--fcd2 | UPDATE specific row(s) | 2022-12-15 | Uaychai Chotjaratwanich
+UPDATE #Culture SET [Name] = 'PingPing' WHERE CultureID = 'pp';
+
+
+--4804 | DELETE specific row(s) | 2022-12-15 | Uaychai Chotjaratwanich
+DELETE #Culture WHERE CultureID='pp';
 
