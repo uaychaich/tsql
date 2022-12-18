df7275ed23b5ad51195fc5c7e43973d9e70287cf
Uaychai Chotjaratwanich <uchotjaratwanich@gmail.com>
2022-12-18 22:23:53 +0700

    2022-12-18

---
+++ Example/22_f4da_control_flow_statement.sql
@@ -0,0 +1,28 @@
+--1343 | PRINT | 2022-12-18 | Uaychai Chotjaratwanich
+PRINT 'Uaychai Chotjaratwanich';
+
+
+--960a | BEGIN...END | 2022-12-18 | Uaychai Chotjaratwanich
+BEGIN PRINT 'Uaychai'; PRINT 'Chotjaratwanich' END
+
+--7a80 | IF | 2022-12-18 | Uaychai Chotjaratwanich
+DECLARE @Var INT = 5;
+IF @Var = 5 PRINT 'Five'; PRINT 'Other';
+GO
+
+
+--8844 | IF...ELSE... | 2022-12-18 | Uayhai Chotjaratwanich
+DECLARE @Var INT = 5;
+IF @Var = 5 PRINT 'Five'; ELSE PRINT 'No five'; PRINT 'Other'
+GO
+
+
+--2382 | IF...ELSE IF...ELSE | 2022-12-18 | Uaychai Chotjaratwanich
+DECLARE @Var INT = 2;
+IF @Var = 1 PRINT 'One'; ELSE IF @Var = 2 PRINT 'Two'; ELSE IF @Var = 3 PRINT 'Three'; ELSE PRINT 'Other';
+GO
+
+
+--4ecd | WHILE | 2022-12-18 | Uaychai Chotjaratwanich
+DECLARE @Var INT = 1;
+WHILE @Var <= 3 BEGIN PRINT @Var; SET @Var = @Var+1; END 
